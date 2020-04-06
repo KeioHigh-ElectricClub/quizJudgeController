b42178835763c7b30299a897bb0ff2de31788346
Oyaki <oc.yaki122@gmail.com>
2020-04-06 11:49:47 +0900

    ResultのSD書き込みを実装

--- lib/quizControll/infrastructure/SD/SdReposiroty.h
+++ lib/quizControll/infrastructure/SD/SdReposiroty.h
@@ -7,13 +7,22 @@
 #include <memory>
 
 #include "domain/Recode/IResultRepository.h"
-#include "domain/Recode/Recode.h"
+#include "domain/Recode/Result.h"
 
 class SdRepository : public IResultRepository {
  public:
-  SdRepository();
+  SdRepository(byte cs, byte cd = 0xff, byte wp = 0xff);
   ~SdRepository();
 
-  bool store(std::unique_ptr<Recode> result);
+  bool store(std::unique_ptr<Result> result);
   bool init();
+
+ private:
+  byte cs = 0, cd = 0xff, wp = 0xff;
+  String nowFileName;
+  int questionNum = 0;
+
+  String createFileName(int num);
+  bool isConnected();
+  bool canWrite();
 };
--- lib/quizControll/infrastructure/SD/SdRepository.cpp
+++ lib/quizControll/infrastructure/SD/SdRepository.cpp
@@ -1,3 +1,60 @@
 #include "SdReposiroty.h"
 
-SdRepository::SdRepository() {}
+SdRepository::SdRepository(byte cs, byte cd, byte wp) {
+  this->cs = cs;
+  if (cd != 0xff) this->cd = cd;
+  if (wp != 0xff) this->wp = wp;
+
+  pinMode(cd, INPUT_PULLUP);
+  pinMode(wp, INPUT_PULLUP);
+}
+
+bool SdRepository::init() {
+  if (!(isConnected() || canWrite())) return false;
+
+  if (!SD.begin(cs, SPI, 24000000)) return false;
+  int counter = 1;
+  while (true) {
+    if (!SD.exists(createFileName(counter))) break;
+    counter++;
+  }
+  nowFileName = createFileName(counter);
+  File newFile = SD.open(nowFileName, FILE_WRITE);
+  newFile.println("問題番号,回答者番号,結果");
+  newFile.close();
+  SD.end();
+
+  questionNum = 0;
+
+  return true;
+}
+
+bool SdRepository::store(std::unique_ptr<Result> result) {
+  byte respondentNum = result->getRespondentNum();
+  bool erratum = result->getErratum() == Erratum::CORRECT;
+
+  char buffer[15];
+  sprintf(buffer, "%d,%d,%d", questionNum, respondentNum, erratum);
+
+  if (!(isConnected() || canWrite())) return false;
+  if (!SD.begin(cs, SPI, 24000000)) return false;
+  File file = SD.open(nowFileName, FILE_APPEND);
+  file.println(buffer);
+  file.close();
+  SD.end();
+
+  if (erratum) questionNum++;
+}
+
+String SdRepository::createFileName(int num) {
+  String fileName = "LOG";
+  if (num < 100) fileName += "0";
+  if (num < 10) fileName += "0";
+  fileName += num;
+  fileName += ".csv";
+  return fileName;
+}
+
+bool SdRepository::isConnected() { return !digitalRead(cd); }
+
+bool SdRepository::canWrite() { return !digitalRead(wp); }
